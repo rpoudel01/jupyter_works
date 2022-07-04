ce4ef5a6d5159ffe17303f5ccdefac2d9fcfc66f
rpoudel01 <rameshrocksy2k@gmail.com>
2022-07-03 17:17:25 -1000

    Add files via upload

---
+++ 1markdown.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # "My Jupyter Notebook on IBM Watson Studio" 
+
+# ## Ramesh Poudel
+#     
+#    I am currently a student in data analytics and i desire to become a data scientist some day.
+
+# _I am interested in data science because i like data and visualizing the data insights using data visualization tools to make correct decisions for the future in any areas of business. Way to render italics is using underscore  sign or the asterisk sign in the begining and no gap after that and another aastersk sign or underscore sign at the end_
+
+# *Also, in better decision making in predictive modelling.*
+
+# ### following code will calculate the sum
+
+# In[2]:
+
+
+1 +1 
+
+
+# ## use three or more astersisk or hyphen sign to make a horizontal rule
+
+# ******
+
+# ## using asterisk and a space after that will give a bullet list ##
+
+# ### Bullet lists for the List of different programming languages: 
+#  
+
+# 
+# * python
+# * Javascript
+# * Java
+# * ruby
+# 
+# 
+
+# # Heading 
+# ## Subheading 
+# ### subsub heading 
+# ##### etc
+
+#  # insert numbered lists
+#  
+#  1. Item 1 
+#  2. item 2 
+#  3. Item 4 
+#          
+#          A. Subitem first
+#          B. Subitem second 
+#  
+# 
+# 
+
+# # Link
+# 
+# Make hyper link like this [link text ] (https: anysite.com) 
+# 
+# for example [link text] (https:www.google.com)
+
+# You can also link to the locations inside the document to make it easier to navigate 
+# 
+# * put an html anchor at the target location
+#   * < a id = 'my_target_name'></a>
+#     
+#     Now put the link to the target anywhere else inthe notebook: 
+#         * [link text] (#my_target_name)
+#             
+
+# In[5]:
+
+
+<u> will get u underlined quote
+
+<u>underline this</u>
+
+
+# drag and drop images to the markdown cells to attach it in the notebook 
+# 
+# ![save.jpg](attachment:save.jpg)
+# 
+# 
+
+# Front color is blue
+
+# mathemeaticalsymbols
+# 
+# 
+
+# Link [with text] lets say for a word my link attach youtube.com follow 
+# 
+# [mylink ](https://www.youtube.com/mylink)
+# 
+
+# link without a text 
+# 
+# https://www.youtube.com/rames
+#     
+
+# link with a text and a tool tip (a message)
+#  
+# [mydata](https://www.youtube.com/mydata "please subscribe")
+# 
+# 
+
+# inserting table in the markdown cell
+# 
+# # Table
+# 
+# 
+
+# |Name| |Age| |basic salary|
+# |-----| |---| |-----------|
+# |Anil| |23|   |33|
+# |susu| |43|   |330|
+# 
+
+# # Code highlighting 
+# 
+# use Backsticks single e.g
+# 
+# `my list = ['databro', 1, true]`
+# 
+# or using htmlstyle.
+# 
+# <code> Python
+# my list = ['databro', 1,true]
+# </code>
+
+# In[3]:
+
+
+import numpy as np
+print("sample")
+
+
+# # blockquote  (using > sign ) 
+# 
+# > this is my first line 
+# >> This is my second line
+# >>> This is my third line 
+# 
+
+# <blockquote >
+# This is just my first line 
+# </blockquote>
+
+# # Adding a strikethrough (adding two tilde ~~ symbols both before and after the characters to be strikethrough )
+# 
+# my name is rames and ~~i am an aspiring data explorer scientist.~~ I am an aspiring data scientist. 
+# 
+# 
+
+# In[ ]:
+
+
+
+
